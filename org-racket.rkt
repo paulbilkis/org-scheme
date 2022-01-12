0126cd7d66599dcfca6a8c01ae0a0c44274fa54d
Pavel Bilkis <p.bilkis@gmail.com>
2022-01-12 22:52:31 +0200

    Add unit testing, remove leading whitespaces after asterics, fix empty-header bug(s)

--- org-racket.rkt
+++ org-racket.rkt
@@ -9,41 +9,22 @@
 ;; returns the depth of a header
 (define (org-level org-line)
   (define (org-level-step l c)
-    (if [equal? (car l) #\*]
-        (org-level-step (cdr l) (add1 c))
-        c))
+    (if (empty? l)
+        c
+        (if [equal? (car l) #\*]
+            (org-level-step (cdr l) (add1 c))
+            c)))
   (org-level-step (string->list org-line) 0))
-(org-level "** ers")
-(org-level "ers")
 
-;; retrieves continious org headers from l, that are lower than n
-(define (retrieve-lower-levels l n)
-  (define (is-lower e)
-    [> (org-level e) n])
-  (define (retrieve-lower-levels-rec l r n)    
-    (if [empty? l]
-        r
-        [if (is-lower [car l])
-            (retrieve-lower-levels-rec [cdr l] [cons (car l) r] n)
-            r])
-    )
-  [reverse (retrieve-lower-levels-rec l '() n)])
-(retrieve-lower-levels  '("** srsr" "** ersr" "ers" "* ers") 1)
+
 
 ;; org-header predicate
 (define (org-header? org-str)
   (> (org-level org-str) 0))
-(org-header? "** res")
-(org-header? "*res")
-(org-header? "res")
 
 ;; extract just the header, without the asterics
 (define (org-extract-header org-str)
   (list->string (drop (string->list org-str) (org-level org-str))))
-
-(org-extract-header "** res")
-(org-extract-header "**res")
-(org-extract-header "res")
 
 ;; reads an org file and convert it into an sexp
 ;; basically reads an org file and calls org->sexp
@@ -56,19 +37,38 @@
       (filter non-empty-string? (read-org fn))
       '[]
       ))
-(parse-org-file "test.org")
 
 ;; format applied after removing asterics, before string is put into final sexp
 ;; this function may be used in the future for e.g removal of leading whitespaces
 (define (org-header-format-str-sexp s)
-  s)
+  (let ((l (string->list s)))
+    (if (empty? l)
+        s
+        (if (equal? (car l) #\space)
+            (org-header-format-str-sexp (list->string (cdr l)))
+            (list->string l))
+        )))
 
 ;; format applied after the string is extracted from sexp and before adding leading asterics
 (define (org-header-format-sexp-str s)
   s)
 
+
+
 ;; takes a list of consequitive org headers as strings, returns sexp representation
 (define (org->sexp org-list)
+  ;; retrieves continious org headers from l, that are lower than n
+  (define (retrieve-lower-levels l n)
+    (define (is-lower e)
+      [> (org-level e) n])
+    (define (retrieve-lower-levels-rec l r n)    
+      (if [empty? l]
+          r
+          [if (is-lower [car l])
+              (retrieve-lower-levels-rec [cdr l] [cons (car l) r] n)
+              r])
+      )
+    [reverse (retrieve-lower-levels-rec l '() n)])
   ;; creates N nested lists with e: e 2 -> ((e))
   ;; this solved the problem, when header of level N is followed by header of level N+M, where M > 1
   ;; this is better than just empty string (e.g. "* header 1\n**\n*** header 3"), because it allows
@@ -89,27 +89,7 @@
                                      (org->sexp-rec reduced (sub1 h)))
                                (- h n 1))
           )))
-  (org->sexp-rec org-list 0))
-
-(org->sexp (filter org-header? (parse-org-file "test.org")))
-(org->sexp (filter org-header? (parse-org-file "todo.org")))
-
-(define (test-sub a)
-  (displayln a)
-  (displayln (org->sexp a))
-  (displayln #\*))
-
-(test-sub (list "* rei rise" "* test1 ste" "** tesr"))
+  (org->sexp-rec (filter org-header? org-list) 0))
 
 ;; takes sexp representation and turns it into org structure
 (define (sexp->org org-sexp) '())
-
-'((" test header level 1")
-  (" another lvl 1" (" second level"
-                     (" third")
-                     (" third2")))
-  (" stand alone lvl1" (
-                        (" wow lvl 3")
-                        (" ")
-                        ))
-  )
---
+++ tests.rkt
@@ -0,0 +1,45 @@
+#lang racket
+
+(require rackunit rackunit/text-ui "org-racket.rkt")
+(require/expose "org-racket.rkt" (org-header? org-level org-extract-header))
+
+
+(define org-sexp-tests
+  (test-suite
+   "org->sexp suite"
+   (test-case "org->sexp"
+     (check-equal? (org->sexp '("* header 1" "* header 1" "** header 2" "non-header"))
+                   '(("header 1") ("header 1" ("header 2"))))
+     (check-equal? (org->sexp '("* header 1" "*** header 3"))
+                   '(("header 1" (("header 3")))))
+     (check-equal? (org->sexp '())
+                   '())
+     (check-equal? (org->sexp (parse-org-file "test.org"))
+                   '(("test header level 1")
+                     ("another lvl 1" ("second level" ("third") ("third2")) ("second again") ("second again x2"))
+                     ("stand alone lvl1" (("wow lvl 3") ("")))
+                     (""))))
+
+   (test-case "org-header"
+     (check-true (org-header? "* a header") #t)
+     (check-false (org-header? "not a header") #f)
+     (check-true (org-header? "*header") #t)
+     (check-true (org-header? "*") #t))
+
+   (test-case "org-level"
+     (check-equal? (org-level "") 0)
+     (check-equal? (org-level "*") 1)
+     (check-equal? (org-level "* ") 1)
+     (check-equal? (org-level "* a header") 1)
+     (check-equal? (org-level "* * a header") 1)
+     (check-equal? (org-level "** a header") 2))
+   ))
+
+(define sexp-org-tests
+  (test-suite
+   "sexp->org suite"
+   (test-case "sexp->org basic" (fail "placeholder")) 
+   ))
+
+(run-tests org-sexp-tests 'normal)
+(run-tests sexp-org-tests 'normal)
